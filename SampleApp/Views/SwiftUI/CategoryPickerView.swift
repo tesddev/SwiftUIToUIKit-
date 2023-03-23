483295c7f2b81a8d7019ad9920e6e61e37cfb1a1
tesddev <tesleem.amuda@gmail.com>
2023-03-23 15:59:57 +0100

    add category picker view

---
+++ SampleApp/Views/SwiftUI/CategoryPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryPickerView.swift
+//  SampleApp
+//
+//  Created by GIGL iOS on 23/03/2023.
+//
+
+import SwiftUI
+
+struct CategoryPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryPickerView()
+    }
+}
